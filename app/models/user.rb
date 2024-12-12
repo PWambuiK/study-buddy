8fe504875d6046e8dc08a7755b0e2ce599549bc6
Patricia Kinuthia <p.wambuikinuthia@gmail.com>
2024-12-12 02:24:29 +0100

    created models and tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :stacks, dependent: :destroy
+  validates :email, :username, :first_name, :last_name, presence: true
+  validates :email, :username, uniqueness: true
 end
